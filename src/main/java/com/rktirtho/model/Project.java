49387835472ef14dfe21c89e84c5bd957853d598
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-08 23:56:29 +0600

    Recommantation class added

--- src/main/java/com/rktirtho/model/Project.java
+++ src/main/java/com/rktirtho/model/Project.java
@@ -1,5 +1,11 @@
 package com.rktirtho.model;
 
 public class Project {
+	private long id;
+	private String title;
+	private String descriptions;
+	private String tools;
+	private String date;
+		
 
 }
---
+++ src/main/java/com/rktirtho/model/Recommandation.java
@@ -0,0 +1,11 @@
+package com.rktirtho.model;
+
+public class Recommandation {
+	
+	private long id;
+	private String author;
+	private String massage;
+	private String date;
+	private String authorDesignation;
+	
+}
