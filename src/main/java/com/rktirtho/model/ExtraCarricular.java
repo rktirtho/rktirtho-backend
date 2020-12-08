ea69235877d4d8cd73bf96287648eb76d0b6bcc3
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-08 23:37:50 +0600

    extra carricular activities model done

--- src/main/java/com/rktirtho/model/ExtraCarricular.java
+++ src/main/java/com/rktirtho/model/ExtraCarricular.java
@@ -1,5 +1,55 @@
 package com.rktirtho.model;
 
 public class ExtraCarricular {
+	private long id;
+	private String title;
+	private String organizations;
+	private String date;
+	
+	
+	public ExtraCarricular() {
+		super();
+	}
+	
+	public ExtraCarricular(String title, String organizations, String date) {
+		super();
+		this.title = title;
+		this.organizations = organizations;
+		this.date = date;
+	}
+
+	public ExtraCarricular(long id, String title, String organizations, String date) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.organizations = organizations;
+		this.date = date;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getOrganizations() {
+		return organizations;
+	}
+	public void setOrganizations(String organizations) {
+		this.organizations = organizations;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
 
 }
