8e524cbcc7fc2dc111854a59df5f1f56e8fea0d5
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-14 23:59:27 +0600

    added geter and seter in Project class also added JPA annotation

--- src/main/java/com/rktirtho/model/Project.java
+++ src/main/java/com/rktirtho/model/Project.java
@@ -1,6 +1,15 @@
 package com.rktirtho.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Table(name = "projects")
+@Entity
 public class Project {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String title;
 	private String descriptions;
