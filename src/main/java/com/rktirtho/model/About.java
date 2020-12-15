98755a914ef336fbbac86a94425f3500fe9cd686
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-15 23:42:50 +0600

    About class changed to Others and class defined

--- src/main/java/com/rktirtho/model/About.java
+++
@@ -1,5 +0,0 @@
-package com.rktirtho.model;
-
-public class About {
-
-}
---
+++ src/main/java/com/rktirtho/model/Others.java
@@ -0,0 +1,43 @@
+package com.rktirtho.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Table;
+
+@Table(name = "others")
+public class Others {
+	private long id;
+	private String kewword;
+	private String text;
+	
+	public Others() {
+		super();
+	}
+	
+	public Others(String kewword, String text) {
+		super();
+		this.kewword = kewword;
+		this.text = text;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getKewword() {
+		return kewword;
+	}
+	public void setKewword(String kewword) {
+		this.kewword = kewword;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
