95ecfa3ff897c2958e19c8da26ee14d197511f30
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-15 23:50:09 +0600

    Training api class added

---
+++ src/main/java/com/rktirtho/api/TrainingApi.java
@@ -0,0 +1,5 @@
+package com.rktirtho.api;
+
+public class TrainingApi {
+
+}
--- src/main/java/com/rktirtho/repository/TrainingRepository.java
+++ src/main/java/com/rktirtho/repository/TrainingRepository.java
@@ -1,5 +1,11 @@
 package com.rktirtho.repository;
 
-public class TrainingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import com.rktirtho.model.Training;
+
+@Component
+public interface TrainingRepository extends JpaRepository<Training, Long>{
 
 }
